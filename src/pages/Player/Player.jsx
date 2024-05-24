ac8ad24ed8d2a5ff98216145f24cace58c540170
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-24 16:57:23 +0200

    updated files

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-import './Player.css'
+import React, { useEffect, useState } from "react";
+import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useParams } from "react-router-dom";
 const Player = () => {
-  return (
-    <div className='player'>Player</div>
-  )
-}
+  const {id} =useParams();
+  const [apiData, setApiData] = useState({
+    name:'',
+   key:'',
+   published_at:'',
+   type:'',
+  })
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZGM0OTQ3MDZhYzA1M2E0MDA5MTQ1MTFiNTZkYjYyMyIsInN1YiI6IjY2NTA3ZmRlZmQ3MTczNDZiNGRkODg5OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bZtzviyvX8-rDOqxQBTUctIUWxpf3s6TA-U9QQQrY5Q",
+    },
+  };
+useEffect(() => {
+  fetch(`https://api.themoviedb.org/3/movie/${id}/videos`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
 
-export default Player
+ 
+}, [])
+
+  return (
+    <div className="player">
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameborder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)} </p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
