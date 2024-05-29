22e543ac432c897b8d44384ce96a1ba54aa9744f
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-29 14:01:33 +0200

    updated files

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { Link } from "react-router-dom";
-
 
 const TitleCards = ({ title, category }) => {
   const [apiData, setApiData] = useState([]);
@@ -16,31 +15,48 @@ const TitleCards = ({ title, category }) => {
     },
   };
 
-  
   const handleWheel = (event) => {
     event.preventDefault();
     cardsRef.current.scrollLeft += event.deltaY;
   };
+
+  const scrollLeft = () => {
+    cardsRef.current.scrollLeft -= 200;
+  };
+
+  const scrollRight = () => {
+    cardsRef.current.scrollLeft += 200;
+  };
+
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/${category?category:'now_playing'}`, options)
-    .then((response) => response.json())
-    .then((response) => setApiData(response.results))
-    .catch((err) => console.error(err));
+    fetch(
+      `https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setApiData(response.results))
+      .catch((err) => console.error(err));
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
+
   return (
     <div className="title-cards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
+      <MdChevronLeft size={40} className="icon icon-left" onClick={scrollLeft} />
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) => {
           return (
             <Link to={`/player/${card.id}`} className="card" key={index}>
-              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+              <img
+                src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path}
+                alt={card.original_title}
+              />
               <p>{card.original_title}</p>
             </Link>
           );
         })}
       </div>
+      <MdChevronRight size={40} className="icon icon-right" onClick={scrollRight} />
     </div>
   );
 };
