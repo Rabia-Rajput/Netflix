a28dbee3258555f211880e9a7919df4ef030f78a
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-29 14:01:59 +0200

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
 import React, { useEffect } from "react";
 import Home from "./pages/Home/Home";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Login from './pages/Login/Login';
+import Login from "./pages/Login/Login";
 import Player from "./pages/Player/Player";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const navigate =useNavigate();
+  const navigate = useNavigate();
   useEffect(() => {
-    onAuthStateChanged(auth, async(user)=>{
-if(user){
-  console.log('login');
-  navigate('/');
-}else{
-  console.log('logout')
-  navigate('/login');
-}
-    }
-  )
-  
-  }, [])
-  
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log("login");
+        navigate("/");
+      } else {
+        console.log("logout");
+        navigate("/login");
+      }
+    });
+  }, []);
+
   return (
     <div>
-         <ToastContainer theme="dark" />
+      <ToastContainer theme="dark" />
       <Routes>
-        <Route path="/" element={<Home />} />   
-        <Route path="/login" element={<Login />} /> 
-        <Route path="/player/:id" element={<Player/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
   );
