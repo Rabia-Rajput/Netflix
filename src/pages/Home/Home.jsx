587af0b9496cad6f85bf2c41688141008d1f10db
Rabia-Rajput <rajput.rabia@outlook.com>
2024-06-07 16:10:55 +0200

    updated files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -37,8 +37,8 @@ const Home = () => {
       <div className="more-cards">
         <TitleCards title={"Blockbuster Movies"} category={"top_rated"} />
         <TitleCards title={"Only on Netflix"} category={"popular"} />
-        <TitleCards title={"Upcoming"} category={"upcoming"} />
         <TitleCards title={"Top Picks For You"} category={"now_playing"} />
+        <TitleCards title={"Upcoming"} category={"upcoming"} />
       </div>
       <Footer />
     </div>
